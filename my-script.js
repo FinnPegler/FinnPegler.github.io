83ffd72c60c06ab33a54d9dbf2896f9c3a8c7a71
Finn Pegler <finnpegler@gmail.com>
2019-11-19 19:27:41 +0000

    Add files via upload

--- my-script.js
+++ my-script.js
@@ -1,21 +1,33 @@
-const box = document.querySelectorAll("#box");
-
+//Task 1
 function changeToOrange(event) {
 	event.target.classList.toggle("orange")
-};
-
-for (var i = 0 ; i < box.length; i++){
-	box[i].addEventListener("click", changeToOrange);
 }
 
 function changeToCyan(event) {
 	event.target.classList.toggle("cyan")
-};
-
-for (var i = 0 ; i < box.length; i++){
-	box[i].addEventListener("mouseover", changeToCyan);
 }
 
 
+const box = document.querySelectorAll("#box")
+
+for (var i = 0 ; i < box.length; i++){
+	box[i].addEventListener("click", changeToOrange)
+}
+
+
+for (var i = 0 ; i < box.length; i++){
+	box[i].addEventListener("mouseover", changeToCyan)
+}
+
+//Task 2
+function changeImg(event) {
+	event.target.img.toggle("hidden");
+}
+
+const imgs = document.querySelectorAll("img");
+
+for (var j = 0 ; j < img.length; j++){
+	imgs[j].addEventListener("click", changeImg);
+}
 
 
