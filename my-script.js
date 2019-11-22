dcda15d767377472c26c698164cc352a9d9da8f1
Finn Pegler <finnpegler@gmail.com>
2019-11-22 23:27:22 +0000

    Add files via upload

--- my-script.js
+++ my-script.js
@@ -47,6 +47,5 @@ else (document.getElementById("carousel-image").src = "img1.jpg") }
 document.getElementById("carousel-image").addEventListener("click", changeImg);
 
 
-//trying to get the function changeImg working, it should be changing the source 
-//but not sure why. I have basically the same thing as this answer 
-// https://stackoverflow.com/questions/6764961/change-an-image-with-onclick
+
+//add left right buttons next
