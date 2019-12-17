588091a8b82b6d3dda292e50aa7793e8cd1c6b74
Finn Pegler <finnpegler@gmail.com>
2019-12-17 21:35:28 +0000

    Add files via upload

--- my-script.js
+++ my-script.js
@@ -1,5 +1,61 @@
+//Hide and show icon in first column (Github)
+function hideIcon1() {
+  document.getElementById("icon1").className = "hidden";
+}
+function showIcon1() {
+  document.getElementById("icon1").className = "shown";
+}
+document.getElementById("col1").addEventListener("mouseleave", hideIcon1);
+document.getElementById("col1").addEventListener("mouseenter", showIcon1);
+
+
+//Hide and show icon in second column (Freecodecamp)
+function hideIcon2() {
+  document.getElementById("icon2").className = "hidden";
+}
+function showIcon2() {
+  document.getElementById("icon2").className = "shown";
+}
+document.getElementById("col2").addEventListener("mouseleave", hideIcon2);
+document.getElementById("col2").addEventListener("mouseenter", showIcon2);
+
+
+//Hide and show icon in first column (Codewars)
+function hideIcon3() {
+  document.getElementById("icon3").className = "hidden";
+}
+function showIcon3() {
+  document.getElementById("icon3").className = "shown";
+}
+document.getElementById("col3").addEventListener("mouseleave", hideIcon3);
+document.getElementById("col3").addEventListener("mouseenter", showIcon3);
+
+
+function centerBoxes() {
+	let elem1 = document.getElementById("threecolumns");
+	let info1 = elem1.getBoundingClientRect();
+	let width1 = info1.width;
+	let elem2 = document.getElementById("col1");
+	let info2 = elem2.getBoundingClientRect();
+	let width2 = info2.width;
+    
+    if (width1 < width2*2 ) {
+       document.getElementById("threecolumns").className = "columnscenter";
+    } else if (width1 < width2*3 ) {
+       document.getElementById("threecolumns").className = "columnsaround"; 
+
+    } else {
+       document.getElementById("threecolumns").className = "threecolumns";
+    }
+}
+
+window.onresize = centerBoxes;
+window.onload = centerBoxes;
+
+
+
 function changeImgFwd() {
-if (document.getElementById("car-image").src.includes("img1.jpg"))
+if (document.getElementById("car-image").src.includes("img1.jpg"))	
 {
    	document.getElementById("car-image").src = "img2.jpg";
    	document.getElementById("circle1").className = "smallbtn";
@@ -62,7 +118,7 @@ function playImgs(){
 }
 
 var count2 = 0;
-function changeColour(){
+function changeOpacity(){
 	if (count2 <1 ){
 		document.getElementById("playbtn").style.opacity = 0.2;
 		document.getElementById("pausebtn").style.opacity = 1;
@@ -77,28 +133,8 @@ function changeColour(){
 
 document.getElementById("playbtn").addEventListener("click", playImgs);
 document.getElementById("pausebtn").addEventListener("click", playImgs);
-document.getElementById("playbtn").addEventListener("click", changeColour);
-document.getElementById("pausebtn").addEventListener("click", changeColour);
+document.getElementById("playbtn").addEventListener("click", changeOpacity);
+document.getElementById("pausebtn").addEventListener("click", changeOpacity);
 
 
 
-function centerBoxes() {
-	let elem1 = document.getElementById("threecolumns");
-	let info1 = elem1.getBoundingClientRect();
-	let width1 = info1.width;
-	let elem2 = document.getElementById("col1");
-	let info2 = elem2.getBoundingClientRect();
-	let width2 = info2.width;
-    
-    if (width1 < width2*2 ) {
-       document.getElementById("threecolumns").className = "columnscenter";
-    } else if (width1 < width2*3 ) {
-       document.getElementById("threecolumns").className = "columnsaround"; 
-
-    } else {
-       document.getElementById("threecolumns").className = "threecolumns";
-    }
-}
-
-window.onresize = centerBoxes;
-window.onload = centerBoxes;
