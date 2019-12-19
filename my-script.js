28fb271115080167a022181eaa67088f55625491
Finn Pegler <finnpegler@gmail.com>
2019-12-19 11:24:23 +0000

    Fixing mobile issues

--- my-script.js
+++ my-script.js
@@ -12,7 +12,7 @@ function exitpopup (){
 document.getElementById("popup").className = "hidden";
 }
 
-document.getElementById("exit").addEventListener("click", exitpopup);
+document.getElementById("exit").addEventListener("click", exitpopup);S
 
 //functions to create the video caption animation
 let str = ""
